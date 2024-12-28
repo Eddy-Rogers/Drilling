ed7afc1d88f94c56e616ace956c5f914547a39fe
Eddy Rogers <eddyrogersiv@gmail.com>
2024-12-28 12:20:08 -0400

    Planet Generation and Drill

--- Planet.cs
+++ Planet.cs
@@ -3,14 +3,19 @@ using System;
 
 public partial class Planet : Node2D
 {
+	private float _maxLayers = 3f;
 
+	private int _maxX;
+	private int _maxY;
 	private TileMapLayer _tilemap;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_maxX = (int)GetMeta("MaxX");
+		_maxY = (int)GetMeta("MaxY");
+		
 		_tilemap = GetNode<TileMapLayer>("PlanetTilemap");
-		GD.Print("Test");
-		Generate(100,100);
+		Generate();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,23 +23,28 @@ public partial class Planet : Node2D
 	{
 	}
 
-	private void Generate(int x, int y)
+	private void Generate()
 	{
-		int xStart = 0 - (x / 2);
-		int xEnd = x / 2;
-		
-		GD.Print(xStart);
-		GD.Print(xEnd);
+		int xStart = 0 - (_maxX / 2);
+		int xEnd = _maxX / 2;
 
 		for (int currentX = xStart; currentX <= xEnd; currentX++)
 		{
-			GD.Print(currentX);
-			for (int currentY = 0; currentY < y; currentY++)
+			for (int currentY = 0; currentY < _maxY; currentY++)
 			{
-				GD.Print(currentY);
-				_tilemap.SetCell(new Vector2I(currentX, currentY), 0, new Vector2I(0, 0), 0);
+				_tilemap.SetCell(new Vector2I(currentX, currentY), 0, DetermineTile(currentY), 0);
 			}
 		}
+	}
+
+	private Vector2I DetermineTile(int y)
+	{
+		// Upper: 0,0
+		// Middle: 0,1
+		// Lower: 0,2
+		int currentLayer = (int)Math.Truncate(((float)y / (float)_maxY) * _maxLayers);
+		
+		return new Vector2I(0, currentLayer);
 	}
 	
 }
